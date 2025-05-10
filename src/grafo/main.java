183b73ed31002b70a3782eaa0ade21e792705bf6
Moy446 <a22310402@ceti.mx>
2025-05-09 23:55:13 -0600

    grafo para IA

--- src/grafo/main.java
+++ src/grafo/main.java
@@ -5,38 +5,54 @@ public class main {
     public static void main(String[] args) {
 
         
-        Nodo nodoA = new Nodo(11);
-        Nodo nodoB = new Nodo(12);
-        Nodo nodoC = new Nodo(13);
-        Nodo nodoD = new Nodo(14);
-        Nodo nodoE = new Nodo(15);
-        Nodo nodoF = new Nodo(16);
+        Nodo nodo0 = new Nodo(0);
+        Nodo nodo1 = new Nodo(1);
+        Nodo nodo2 = new Nodo(2);
+        Nodo nodo3 = new Nodo(3);
+        Nodo nodo4 = new Nodo(4);
+        Nodo nodo5 = new Nodo(5);
+        Nodo nodo6 = new Nodo(6);
+        Nodo nodo7 = new Nodo(7);
+        Nodo nodo8 = new Nodo(8);
         
-        grafo.newNodo(nodoA);
-        grafo.newNodo(nodoB);
-        grafo.newNodo(nodoC);
-        grafo.newNodo(nodoD);
-        grafo.newNodo(nodoE);
-        grafo.newNodo(nodoF);
+        grafo.newNodo(nodo0);
+        grafo.newNodo(nodo1);
+        grafo.newNodo(nodo2);
+        grafo.newNodo(nodo3);
+        grafo.newNodo(nodo4);
+        grafo.newNodo(nodo5);
+        grafo.newNodo(nodo6);
+        grafo.newNodo(nodo7);
+        grafo.newNodo(nodo8);
         
-        origenDestino(nodoA, nodoB, 2);
+        // direcciones 0
+        origenDestino(nodo0, nodo1, 4);
+        origenDestino(nodo0, nodo7, 8);
+        // direcciones 1
+        origenDestino(nodo1, nodo2, 8);
+        origenDestino(nodo1, nodo7, 11);
+        // direcciones 2 
+        origenDestino(nodo2, nodo3, 7);
+        origenDestino(nodo2, nodo8, 2);
+        origenDestino(nodo2, nodo5, 4);
+        // direcciones 3
+        origenDestino(nodo3, nodo4, 9);
+        origenDestino(nodo3, nodo5, 14);
+        // direcciones 4
+        origenDestino(nodo4, nodo5, 10);
+        // direcciones 5
+        origenDestino(nodo5, nodo6, 2);
+        // direcciones 6
+        origenDestino(nodo6, nodo8, 6);
+        origenDestino(nodo6, nodo7, 1);
+        // direcciones 7
+        origenDestino(nodo7, nodo8, 7);
         
-        origenDestino(nodoB, nodoC, 7);
-        origenDestino(nodoB, nodoD, 6);
-        origenDestino(nodoB, nodoE, 10);
-        origenDestino(nodoB, nodoF, 2);
-        
-        origenDestino(nodoC, nodoD, 4);
-        origenDestino(nodoC, nodoF, 1);
-        
-        origenDestino(nodoD, nodoE, 3);
-        
-        origenDestino(nodoE, nodoF, 5);
         
         System.out.println(grafo.mostrar());
         System.out.println(grafo.mostrarP());
         
-        System.out.println(grafo.buscar(grafo, nodoD, nodoF));
+        System.out.println(grafo.buscar(grafo, nodo0, nodo4));
     }
     
     public static void origenDestino (Nodo origen, Nodo destino, int Peso){
